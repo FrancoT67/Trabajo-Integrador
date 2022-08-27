02226500153c7f89301567612cf2ecaf34bd73ee
Franco Toledo <francotoledo1235@gmail.com>
2022-08-26 23:39:13 -0300

    correccion de la base de datos

--- BD/BaseDedatos.sql
+++ BD/BaseDedatos.sql
@@ -10,7 +10,7 @@ nota1 decimal(2,2),
 nota2 decimal(2,2),
 nota3 decimal(2,2),
 estado VARCHAR(30),
-cod_materia INT not null,
+cod_materia INT NOT NULL AUTO_INCREMENT,
 PRIMARY KEY(cod_materia));
 
 CREATE TABLE Alumno(
@@ -19,8 +19,7 @@ DNI VARCHAR(9) NOT NULL ,
 Curso VARCHAR (8) NOT NULL,
 Telefono INT(10),
 Email VARCHAR (20),
-division varchar(6),
-cod_materia INT not NULL,
-foreign KEY(cod_materia) references materias(cod_materia),
-PRIMARY KEY (DNI)
+cod_alumno INT NOT NULL AUTO_INCREMENT,
+foreign KEY(cod_alumno) references materias(cod_alumno),
+PRIMARY KEY (cod_alumno)
 );
