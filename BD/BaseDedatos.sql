6570979f7fb60431abf9cf929859fd1f23ccb41e
Franco Toledo <francotoledo1235@gmail.com>
2022-08-28 20:12:22 -0300

    correccion de la BaseDedatos

--- BD/BaseDedatos.sql
+++ BD/BaseDedatos.sql
@@ -5,21 +5,22 @@ CREATE DATABASE prueba;
 USE prueba;
 
 create table materias(
-nombreM VARCHAR(255),
-nota1 decimal(2,2),
-nota2 decimal(2,2),
-nota3 decimal(2,2),
-estado VARCHAR(30),
+nombreM VARCHAR(255) NOT NULL,
+nota1 decimal(2,2)NOT NULL,
+nota2 decimal(2,2) NOT NULL,
+nota3 decimal(2,2) NOT NULL,
+estado VARCHAR(30) NOT NULL,
 cod_materia INT NOT NULL AUTO_INCREMENT,
 PRIMARY KEY(cod_materia));
 
-CREATE TABLE Alumno(
-Nombre VARCHAR(30) NOT NULL,
-DNI VARCHAR(9) NOT NULL ,
-Curso VARCHAR (8) NOT NULL,
-Telefono INT(10),
-Email VARCHAR (20),
+CREATE TABLE alumno(
+nombre VARCHAR(30) NOT NULL,
+dni VARCHAR(9) NOT NULL ,
+curso VARCHAR (8) NOT NULL,
+telefono INT(10) NOT NULL,
+email VARCHAR (20) NOT NULL,
+cod_materia INT AUTO_INCREMENT,
 cod_alumno INT NOT NULL AUTO_INCREMENT,
-foreign KEY(cod_alumno) references materias(cod_alumno),
+foreign KEY(cod_materia) references materias(cod_materia),
 PRIMARY KEY (cod_alumno)
 );
